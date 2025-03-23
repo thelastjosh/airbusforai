95decb4b03ac7d9946e90504ee2c6650d4f83d96
Joshua Tan <joshua.z.tan@gmail.com>
2025-03-23 15:07:49 +0800

    initial commit

---
+++ components/Head.js
@@ -0,0 +1,34 @@
+import Head from 'next/head';
+
+const TITLE = "Airbus for AI"
+const DESC = "A global strategy for public value creation."
+
+export default function HeadComponent() {
+  return <Head>
+    <title>{TITLE}</title>
+    <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;0,600;1,400&family=Roboto+Mono&display=swap" rel="stylesheet" />
+
+    {/* Place any emoji as text wrapped in svg for favicon */}
+    <link rel="icon"
+      href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22> 
+      ✍️ 
+      </text></svg>"/>
+
+    <meta property="og:title" content={TITLE} />
+    <meta property="og:description" content={DESC} />
+    <meta property="og:image" content="/og-image.png" />
+    <meta property="og:url" content="https://airbusforai.org" />
+
+    // <meta name="twitter:card" content="summary" />
+    // <meta property="og:type" content="website" />
+    // <meta property="og:title" content="Airbus for AI" />
+    // <meta property="og:description" content="The Airbus for AI proposal." />
+    // <meta property="og:image" content="/og-image.png" />
+
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content="https://airbusforai.org" />
+    <meta property="twitter:title" content="Airbus for AI" />
+    <meta property="twitter:description" content="A global strategy for public value creation." />
+    <meta property="twitter:image" content="https://airbusforai.org/og-image.png" />
+  </Head>
+}
---
+++ components/Sidebar.js
@@ -0,0 +1,55 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useState, useEffect, useRef } from "react"
+import { IoIosMenu } from "react-icons/io";
+import useOutsideClick from "../hooks/useOutsideClick"
+
+const LINKS = [
+  { label: "Manifesto", href: "/" },
+  { label: "Commentaries", href: "/commentaries" },
+  { label: "Discussion", href: "https://signal.group/#CjQKIGr0eKL8jL4icLLq5HrWQsWyvEtt8MhXGSsBDliC4gINEhBEy7q_0Axd2OvLToiQG3cF" },
+  { label: "About", href: "/about" },
+]
+
+export default function Sidebar() {
+  const router = useRouter()
+  const [open, setOpen] = useState(true)
+  const ref = useRef(null);
+
+  useOutsideClick(ref, () => {
+    if (open) {
+      setOpen(false);
+    }
+  });
+
+  const calculateBg = (href) => (
+    router.pathname === href ||
+    (router.pathname === "" && href === "/")
+  ) ? 'bg-gray-hover' : 'bg-gray-wash'
+
+  const handleResize = () => {
+    setOpen(window.innerWidth >= 1280)
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => window.removeEventListener('resize', handleResize);
+  }, [])
+
+  const bg = open ? "bg-gray-wash border-r border-gray" : ""
+
+  return <div className={`h-[100vh] z-[9999] flex flex-col fixed justify-center font-body text-xl xl:bg-transparent xl:border-none ${bg}`}>
+    <IoIosMenu className="absolute left-0 top-0 m-8 xl:hidden block cursor-pointer" size={40} onClick={() => setOpen(state => !state)} />
+    <div className={open ? "px-32" : "hidden pointer-events-none"} ref={ref}>
+      <h2 className="font-bold">Table of Contents</h2>
+      <ol className="list-[upper-roman] ml-4 my-4">
+        {LINKS.map((link, i) => <li key={i} className="my-2">
+          <Link href={link.href}>
+            <a className={`no-underline rounded-md ${calculateBg(link.href)} hover:bg-gray-hover transition ease-in-out px-1`}>{link.label}</a>
+          </Link>
+        </li>)}
+      </ol>
+    </div>
+  </div>
+}
